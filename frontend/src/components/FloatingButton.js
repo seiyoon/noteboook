ae57d2f00ec7751dac4bdeff5d593bdffa8e032a
seiyoon <sera4084@naver.com>
2023-05-06 23:38:49 +0900

    chore: 플로팅 버튼 수정

--- frontend/src/components/FloatingButton.js
+++ frontend/src/components/FloatingButton.js
@@ -1,12 +1,36 @@
-import React from "react";
-import "../css/FloatingButton.css";
+import styled from "styled-components";
+import { COLOR } from "../styles/color";
 
 const FloatingButton = ({ onClick }) => {
   return (
-    <button className="floatingButton" onClick={onClick}>
-      +
-    </button>
+    <StFloatingButton onClick={onClick}>
+      <img
+        src={require("../assets/pencil.png")}
+        className="logo"
+        alt="mainLogo"
+      />
+    </StFloatingButton>
   );
 };
 
 export default FloatingButton;
+
+const StFloatingButton = styled.button`
+  position: fixed;
+  bottom: 70px;
+  right: 100px;
+  width: 100px;
+  height: 100px;
+  background-color: ${COLOR.MAIN};
+  border-radius: 200px;
+  border: none;
+  padding-left: 10px;
+  box-shadow: 1px 5px 5px rgba(0, 0, 0, 0.3);
+  cursor: pointer;
+  :hover {
+    background-color: ${COLOR.MAIN_HOVER};
+  }
+  ${({ theme }) => theme.tablet`
+    right: 50px;
+  `};
+`;
